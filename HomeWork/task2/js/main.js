d9340b1f73fe3b37e60e9b59f4f4dc824b10286e
rytashygyda <ryta.shygyda@unity-bars.com>
2016-04-07 14:53:13 +0300

    Add switch construction

---
+++ HomeWork/task2/js/main.js
@@ -0,0 +1,21 @@
+﻿var user = {
+    name: "Ryta",
+    email: "ryta@mail.com",
+    age: undefined,
+    isAdmin: false
+};
+
+// using construction case
+age = Math.round(Math.random() * 100);
+switch (true) {
+    case age % 10 == 1:
+        console.log("Користувачу " + name + " " + age + " рік");
+        break;
+    case ((age % 10 >= 2) && (age % 10 <= 4)):
+        console.log("Користувачу " + name + " " + age + " роки");
+        break;
+
+    default:
+        console.log("Користувачу " + name + " " + age + " років");
+        break;
+}
