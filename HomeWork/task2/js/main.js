fa10fd3d965a80e1cb5b1ef338de8a0496bb0f91
rytashygyda <ryta.shygyda@unity-bars.com>
2016-04-11 20:10:45 +0300

    Show user name

--- HomeWork/task2/js/main.js
+++ HomeWork/task2/js/main.js
@@ -11,25 +11,25 @@ console.log("*** Using construction switch ***");
 age = Math.round(Math.random() * 100);
 switch (true) {
     case age % 10 == 1:
-        console.log("Користувачу " + name + " " + age + " рік");
+        console.log("Користувачу " + user.name + " " + age + " рік");
         break;
     case ((age % 10 >= 2) && (age % 10 <= 4)):
-        console.log("Користувачу " + name + " " + age + " 'роки");
+        console.log("Користувачу " + user.name  + " " + age + " 'роки");
         break;
 
     default:
-        console.log("Користувачу " + name + " " + age + " років");
+        console.log("Користувачу " + user.name  + " " + age + " років");
         break;
 }
 // using construction if
 console.log("*** Using construction if ***");
 age = Math.round(Math.random() * 100);
 if ( age % 10 == 1 ) {
-     console.log("Користувачу " + name + " " + age + " рік");    
+     console.log("Користувачу " + user.name  + " " + age + " рік");    
 } else if ( (age % 10 >= 2) && (age % 10 <= 4) ) {
-     console.log("Користувачу " + name + " " + age + " роки");
+     console.log("Користувачу " + user.name  + " " + age + " роки");
 }
 else{
-    console.log("Користувачу " + name + " " + age + " років");
+    console.log("Користувачу " + user.name + " " + age + " років");
 }
 
