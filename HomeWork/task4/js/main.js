d4c969aea9f22895722ef550128231e6bbf1e6c1
rytashygyda <ryta.shygyda@unity-bars.com>
2016-04-20 18:44:32 +0300

    Add task 4

---
+++ HomeWork/task4/js/main.js
@@ -0,0 +1,36 @@
+﻿/*
+Створити об’єкт user з загальними властивостями для всіх користувачів
+Створити об’єкт admin котрий наслідується від user та розширяє його.
+Виконати перебір і вивід в консоль властивостей admin пропустивши властивості, що наслідуються
+*/
+var user = {
+		name: null,
+		email: "hello.kittycats.com",
+		age: 0
+	}	
+var admin = {
+		salary: 120,
+        knowledge: 'Oracle',
+        certificat: 'Oracle DBA 1'
+	}	
+ 
+ var manager = {
+     salary: 115,
+     knowledge: 'PMP'
+ }
+admin.__proto__  = user;
+manager.___proto__ = user;
+
+for (var key in admin) {
+    if (admin.hasOwnProperty(key)){
+        console.log( 'My property is ' + key + ' = ' + admin[key]);
+    }
+    else {
+        console.log( 'Not my property is ' + key + ' = ' + admin[key]);
+    }
+ }  
+
+
+
+
+
